fe221e6512779c5c0eaa2f33683cb00f8e21ed15
praveenatla01 <praveenatla01@gmail.com>
2017-02-23 20:40:49 +0530

    Add files via upload

---
+++ oddeven.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	// your code goes here
+
+int a;
+std::cin>>a;
+if(a%2==0)
+std::cout<<"even";
+else
+std::cout<<"odd";
+
+return 0;
+
+}
